7bb25b56fdd37c4074d122d238337b0299dd3b92
John Haberstroh <jhaberstroh@berkeley.edu>
2013-12-18 12:34:09 -0800

    Got multi-line read functioning in matrix class

--- Matrix/LargeMatrix.cpp
+++ Matrix/LargeMatrix.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
 namespace Mtx
 {
@@ -17,6 +18,7 @@ LargeMatrix::LargeMatrix(std::string a_filename, int a_num_rows, Format a_fmt, i
 		m_fmt = a_fmt;
 		m_iostate = a_iostate;
 		m_num_rows = a_num_rows;
+		m_nums_in_memory = a_nums_in_memory;
 		assert(m_iostate | FRead);
 		assert(m_iostate | FWrite);
 		std::cout << "Succeeded assertions!"<<std::endl;
@@ -60,13 +62,52 @@ void LargeMatrix::GenerateRandom(std::string a_filename, int a_num_rows, int a_i
 		gsl_rng_free(r);
 	}
 
+
+
+void LargeMatrix::TxtMult(colVect& output, const colVect& arg)
+	{
+
+		CH_TIMERS("Multiplication");
+		CH_TIMER("mult_file", t1);
+
+		std::ifstream mtx_file;
+		CH_START(t1);
+		mtx_file.open(m_filename.c_str());
+		//std::cout << "MATRIX LOADING: " << std::endl;
+		std::stringstream ss;
+		for (int i = 0 ; i < m_num_rows ; i++)
+			{
+				rowVect row(m_num_rows,0);
+				std::string line;
+
+				if (mtx_file.is_open())
+					{
+						getline(mtx_file,line);
+					}
+				ss.str(line);
+
+				std::string remain;
+				for (int j = 0 ; j < m_num_rows ; j++)
+					{
+						ss >> row[j];
+						//std::cout << row[j] << "  \t";
+						ss.ignore(256,',');
+					}
+				//std::cout << std::endl;
+				
+				output[i] = row*arg;
+			}
+		CH_STOP(t1);
+		mtx_file.close();
+	}
+
 void LargeMatrix::BinMult(colVect& output, const colVect& arg)
 	{
 		CH_TIMERS("Multiplication");
 		CH_TIMER("file_read", t1);
 		CH_TIMER("line_splice", t2);
 		CH_TIMER("vec_multiply", t3);
-		std::cout << "PERFORMING MULTIPLICATION!" << std::endl;
+		//std::cout << "PERFORMING MULTIPLICATION!" << std::endl;
 	
 		std::ifstream mtx_file;
 		mtx_file.open(m_filename.c_str(), std::ifstream::binary);
@@ -111,7 +152,7 @@ void LargeMatrix::BinMult(colVect& output, const colVect& arg)
 					}
 				if (i == 0)
 					{
-						std::cout << std::endl;
+						//std::cout << std::endl;
 					}
 				CH_STOP(t2);
 				
@@ -122,42 +163,87 @@ void LargeMatrix::BinMult(colVect& output, const colVect& arg)
 		mtx_file.close();
 	}
 
-
-
-void LargeMatrix::TxtMult(colVect& output, const colVect& arg)
+void LargeMatrix::OptMult(colVect& output, const colVect& arg)
 	{
-
 		CH_TIMERS("Multiplication");
-		CH_TIMER("mult_file", t1);
-
+		CH_TIMER("trivial", t0);
+		CH_TIMER("file_read", t1);
+		CH_TIMER("line_splice", t2);
+		CH_TIMER("vec_multiply", t3);
+		//std::cout << "PERFORMING MULTIPLICATION!" << std::endl;
+	
 		std::ifstream mtx_file;
-		CH_START(t1);
-		mtx_file.open(m_filename.c_str());
+		mtx_file.open(m_filename.c_str(), std::ifstream::binary);
 		//std::cout << "MATRIX LOADING: " << std::endl;
-		std::stringstream ss;
-		for (int i = 0 ; i < m_num_rows ; i++)
+	
+		int max_lines_read = m_nums_in_memory / m_num_rows;
+		//std::cout << "Max nums in memory: " << m_nums_in_memory << std::endl;
+		//std::cout << "Max lines to read: " << max_lines_read << std::endl;
+
+		int Nchar_double = 8;
+		assert(m_num_rows = 25*25);
+		int line_size = m_num_rows * Nchar_double;
+
+
+		char buffer[ (line_size + 1) * max_lines_read];
+		for (int i = 0 ; i < m_num_rows ; i+=max_lines_read)
 			{
-				rowVect row(m_num_rows,0);
-				std::string line;
-
-				if (mtx_file.is_open())
-					{
-						getline(mtx_file,line);
-					}
-				ss.str(line);
-
-				std::string remain;
-				for (int j = 0 ; j < m_num_rows ; j++)
-					{
-						ss >> row[j];
-						//std::cout << row[j] << "  \t";
-						ss.ignore(256,',');
-					}
-				//std::cout << std::endl;
+				CH_START(t0);
+				assert(mtx_file.is_open());
+				CH_STOP(t0);
 				
-				output[i] = row*arg;
+				CH_START(t1);
+				int num_lines_to_read = std::min(max_lines_read, m_num_rows - i);
+				int num_bytes_to_read = (line_size + 1) * num_lines_to_read;
+
+				//std::cout << "Reading " << num_lines_to_read << " lines.\n Buffer call to read " << num_bytes_to_read << " Bytes." << std::endl;
+	
+				rowVect row(m_num_rows,0);
+				mtx_file.read(buffer, (line_size + 1) * num_lines_to_read);
+				if (!mtx_file)
+					{
+						mtx_file.close();
+						std::cout << "FATAL ERROR: could not read line from file" << std::endl;
+						abort();
+					}
+				CH_STOP(t1);
+	
+				for (int lin = 0 ; lin < num_lines_to_read ; lin++)
+					{
+						CH_START(t2);
+						int line_displace = lin * (line_size + 1);
+						assert(buffer[line_displace + line_size] == '\n');
+						//if (buffer[line_displace + line_size] != '\n')
+						//	{
+						//		std::cout << "Line "<< lin << " failed to pass newline assertion" << std::endl;
+						//		abort();
+						//	}
+						for (int j = 0 ; j < m_num_rows ; j++)
+							{
+								int float_displace = (j * Nchar_double) + line_displace;
+	
+								//assert(displace + Nchar_double  < line_size);
+								row[j] = *(Real*) (buffer + float_displace);
+								//if (i == 0)
+								//	{
+								//		if (j != 0)
+								//			{
+								//				std::cout << ",";
+								//			}
+								//		std::cout << row[j] ;
+								//	}
+							}
+						if (i == 0)
+							{
+								//std::cout << std::endl;
+							}
+						CH_STOP(t2);
+						
+						CH_START(t3);
+						output[i+lin] = row*arg;
+						CH_STOP(t3);
+					}
 			}
-		CH_STOP(t1);
 		mtx_file.close();
 	}
 
@@ -173,6 +259,10 @@ colVect LargeMatrix::operator*(colVect arg)
 		if (m_fmt == FMT_TXT)
 			{
 				TxtMult(dot, arg);
+			}
+		if (m_fmt == FMT_OPT_TEI)
+			{
+				OptMult(dot, arg);
 			}
 		return dot;
 	}
--- Matrix/mtxmain.cpp
+++ Matrix/mtxmain.cpp
@@ -2,48 +2,61 @@
 #include "LargeMatrix.H"
 #include "CH_Timer.H"
 
-
-int main(){
-	CH_TIMERS("MAIN");
-	CH_TIMER("many_mult", t2);
-
-	Mtx::Format fmt = Mtx::FMT_BIN;
+void mtxTest(Mtx::Format fmt)
+	{
 	
-	int rows;
-	std::string mtx_name;
-
-	if (fmt == Mtx::FMT_TXT)
-		{
-			rows = 1000;
-			mtx_name = "newmtx.txt";
-			Mtx::LargeMatrix::GenerateRandom(mtx_name, rows);
-		}
-
-	if (fmt == Mtx::FMT_BIN)
-		{
-			rows = 25 * 25;
-			mtx_name = "../parsing/matx_out_test.dat";
-		}
+		int rows;
+		std::string mtx_name;
 	
+		if (fmt == Mtx::FMT_TXT)
+			{
+				rows = 25*25;
+				mtx_name = "newmtx.txt";
+				Mtx::LargeMatrix::GenerateRandom(mtx_name, rows);
+			}
+	
+		if (fmt == Mtx::FMT_BIN)
+			{
+				rows = 25 * 25;
+				mtx_name = "../parsing/matx_out_test.dat";
+			}
+		
+		if (fmt == Mtx::FMT_OPT_TEI)
+			{
+				rows = 25 * 25;
+				mtx_name = "../parsing/matx_out_test.dat";
+			}
+		
+	
+		Mtx::LargeMatrix x(mtx_name, rows, fmt);
+		Mtx::colVect a(rows, 0);
+		std::cout << "A size: " << a.size() << std::endl;
+		for (int i = 0 ; i < a.size() ; i++)
+			{	
+				a[i] = ((i + 7) * ((i+4) * 123)) % 37  * 111 % 10;
+			}
+		Mtx::print(a);
+	
+	
+		Mtx::colVect b(a);
+		std::cout << "B size: " << b.size() << std::endl;
+		for (int i = 0 ; i < 100 ; i++)
+			{
+				b = x*b;
+			}
+		Mtx::print(b);
+	}
 
-	Mtx::LargeMatrix x(mtx_name, rows);
-	Mtx::colVect a(rows, 0);
-	std::cout << "A size: " << a.size() << std::endl;
-	for (int i = 0 ; i < a.size() ; i++)
-		{	
-			a[i] = ((i + 7) * ((i+4) * 123)) % 37  * 111 % 10;
-		}
-	Mtx::print(a);
 
-
-	Mtx::colVect b(a);
-	std::cout << "B size: " << b.size() << std::endl;
-	CH_START(t2);
-	for (int i = 0 ; i < 100 ; i++)
-		{
-			b = x*b;
-		}
-	CH_STOP(t2);
-	Mtx::print(b);
-
-}
+int main()
+	{
+		CH_TIMERS("MAIN");
+		CH_TIMER("many_mult_bin", t1);
+		CH_TIMER("many_mult_opt", t2);
+		CH_START(t1);
+		mtxTest(Mtx::FMT_BIN);
+		CH_STOP(t1);
+		CH_START(t2);
+		mtxTest(Mtx::FMT_OPT_TEI);
+		CH_STOP(t2);
+	}
